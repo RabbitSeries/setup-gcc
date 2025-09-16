8d339e3268d955c63ece6959eae120659183bb0c
RabbitXWindows <xiali123.ch@gmail.com>
2025-09-16 19:06:15 +0800

    change to cjs

--- esbuild.ts
+++ esbuild.ts
@@ -1,22 +1,26 @@
-import * as esbuild from "esbuild"
+import esbuild from "esbuild"
 import fg from "fast-glob"
 import fs from "node:fs"
+
 const src = fg.globSync(["src/*.ts", "!node_modules"])
 console.log(`Found files: ${src}`)
 const outDirectory = "dist";
 if (fs.existsSync(outDirectory)) {
     fs.rmSync(outDirectory, { force: true, recursive: true })
 }
-await esbuild.build({
-    target: "esnext",
-    entryPoints: src,
-    platform: "node",
-    format: "cjs",
-    bundle: true,
-    outbase: "src",
-    outdir: outDirectory,
-    outExtension: { ".js": ".cjs" },
-    sourcemap: "inline"
-    // treeShaking: true // true if bundle or format is iife
-})
-console.log(`bundle finished`)
+
+async function build() {
+    await esbuild.build({
+        target: "esnext",
+        entryPoints: src,
+        platform: "node",
+        format: "cjs",
+        bundle: true,
+        outbase: "src",
+        outdir: outDirectory,
+        sourcemap: false
+        // treeShaking: true // true if bundle or format is iife
+    })
+    console.log(`bundle finished`)
+}
+build()
